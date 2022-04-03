8eca6172a38afdbf9a86c04da6b4a8a542fa4cf4
jffasna <fasna@rhino-partners.com>
2022-04-03 23:07:47 +0530

    migrations created

---
+++ database/migrations/2022_04_03_173708_create_clients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('surname');
+            $table->string('email')->unique();
+            $table->string('profile_picture');
+            $table->integer('admin_id');
+            $table->timestamps();
+            $table->index('admin_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
